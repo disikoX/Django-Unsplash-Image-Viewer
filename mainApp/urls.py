95711df39829ea1d5826aa09f54bb6b30c55be7e
disikoX <68564251+disikoX@users.noreply.github.com>
2023-09-20 12:54:25 +0200

    Added some spaghetti code for communicating with the API

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -8,5 +8,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path("",views.index,name='index'), 
     path("formpage/",views.form_name_view,name ='form_name'),
+    path("Image",views.Image),
 ]
 urlpatterns += staticfiles_urlpatterns("/static")
--- mainApp/views.py
+++ mainApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from . import forms
 from django.http import HttpResponse
 from .models import User
+import requests
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -20,3 +22,34 @@ def form_name_view(request):
     
     
     return render(request,'templates/mainApp/form_page.html',{'form':form}) #Nom du fichier faux
+
+#def Image(self,request,image):
+    url ="https://api.unsplash.com?q={}&appid=<Pydg9u8CGnUZ3VVpgrIXPU8Yrj77i2pfCvvIlJXyicc>".format(image)
+    response = requests.get(url)
+    data = response.json()
+    return JsonResponse(data)
+
+def Image(request):
+    # Get the query parameter 'q' from the request's GET parameters
+    query = request.GET.get('q', '')  # Default to empty string if 'q' is not provided
+
+    # Construct the URL with the API key in the request headers
+    url = "https://api.unsplash.com/search/photos"
+    headers = {
+        "Authorization": "Client-ID <wwxWk4-za9SCfQ09DnDGFLPEb9J9_5mLRSkRlikkD1Q>"
+    }
+    params = {
+        "query": query
+    }
+
+    # Send the GET request to Unsplash API
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return JsonResponse(data)
+    else:
+        # Handle API error responses here
+        error_data = {"error": "Unable to fetch images"}
+        return JsonResponse(error_data, status=response.status_code)
+
